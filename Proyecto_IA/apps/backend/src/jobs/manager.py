30addd0b8b17dd293c6444f0669560092f6f5dfc
nachorod1-jpg <nachorod1@gmail.com>
2025-12-30 20:24:54 +0100

    act2

--- Proyecto_IA/apps/backend/src/jobs/manager.py
+++ Proyecto_IA/apps/backend/src/jobs/manager.py
@@ -230,10 +230,12 @@ class JobManager:
         session.commit()
 
     def launch_job(self, job: Job):
-        cancel_event = threading.Event()
-        self.active_jobs[job.id] = cancel_event
-        thread = threading.Thread(target=self._run_job, args=(job.id,), daemon=True)
-        thread.start()
+        #cancel_event = threading.Event()
+        #self.active_jobs[job.id] = cancel_event
+        #thread = threading.Thread(target=self._run_job, args=(job.id,), daemon=True)
+        #thread.start()
+        self.active_jobs[job.id] = threading.Event()
+        self._run_job(job.id)
 
     def cancel(self, job_id: int) -> bool:
         event = self.active_jobs.get(job_id)
